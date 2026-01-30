3bcd855df16defae05b694b28b0281b1a1ad7491
codewilson64 <wilsongambit@gmail.com>
2026-01-30 11:42:50 +0700

    edit ux

--- src/app/(root)/resume/(editor)/contact/page.tsx
+++ src/app/(root)/resume/(editor)/contact/page.tsx
@@ -8,7 +8,7 @@ export default function ContactPage() {
   return (
     <div className="min-h-screen flex justify-center font-poppins px-5 lg:px-0 py-12 bg-slate-50 relative">
       
-      <div className="w-full max-w-2xl space-y-10 pb-32">
+      <div className="w-full max-w-2xl space-y-10 pb-40">
         <div className="space-y-2">
           <h1 className="text-2xl font-semibold text-gray-900">Contact Details</h1>
           <p className="text-lg text-gray-600">
@@ -21,7 +21,7 @@ export default function ContactPage() {
       </div>
 
       {/* Fixed Bottom Button */}
-      <div className="max-w-2xl mx-auto absolute bottom-0 left-0 right-0 bg-gray-50 px-5 lg:px-0 py-4">
+      <div className="xl:max-w-2xl xl:mx-auto xl:absolute fixed bottom-0 left-0 right-0 bg-gray-50 px-5 lg:px-0 py-4">
         <button
           onClick={() => router.push("/resume/experience")}
           className="w-full bg-cyan-400 hover:bg-cyan-500 text-white py-4 rounded-lg font-medium transition duration-200"
